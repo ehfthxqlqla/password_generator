bcc74adb2c7a0f7f31103070c3ec750c77d5ac91
손재경 <90304739+ehfthxqlqla@users.noreply.github.com>
2023-03-26 23:46:13 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,14 +3,14 @@ number = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'],
 useChar = document.querySelector("#alphabet"),
 submit = document.querySelector("body > div > div.options > button"),
 len = document.querySelector("body > div > div.options > input"),
-resultText = document.querySelector(".result-text")
+resultText = document.querySelector(".result-text"),
+resultInput = document.querySelector(".result-input"),
+lastpwInput = document.querySelector(".last-password")
 
 let result = ""
 let isExecutedBefore = false
 
 submit.addEventListener("click", function() {
-    // console.log("hi")
-
     if (isExecutedBefore) {
         result = ""
         isExecutedBefore = false
@@ -18,7 +18,6 @@ submit.addEventListener("click", function() {
 
     if (useChar.checked) {
         result = char[Math.floor(Math.random() * char.length)]
-        console.log(result)
     }
 
     for (let i = 0; i < Math.abs(len.value); i++) {
@@ -33,18 +32,38 @@ submit.addEventListener("click", function() {
         return
     }
 
+    resultInput.value = `${result}`
+    window.localStorage.setItem("pw", result)
+    lastpwInput.value = `${window.localStorage.getItem("pw")}`
+
     if (document.querySelector("#clipboard").checked) {
-        if (window.confirm(`생성한 결과를 클립보드에 저장하시겠습니까?`)) {
+        if (window.confirm(`생성한 결과를 클립보드에 저장하고 비밀번호를 설정하시겠습니까?`)) {
             navigator.clipboard.writeText(result)
             .then(() => {
                 alert("성공적으로 복사했습니다.")
             })
-            .catch((error) => {
-                alert(`실패했습니다. 수동으로 복사해주세요.`)
-                console.error(error)
+            .catch(() => {
+                try {
+                    resultInput.select()
+                    resultInput.setSelectionRange(0, 100000)
+                    document.execCommand("copy")
+                    alert(`성공적으로 복사했습니다.`)
+                } catch {
+                    alert(`실패했습니다.`)
+                }
             })
         }
-    }
 
-    resultText.innerText = `생성 결과 : ${result}`
+        (async() => {
+            await axios.post(`https://api.ehfthxqlqla.repl.co/api/v1/pw/generate`, {
+                password:result
+            })
+            .then(() => {
+                alert(`비밀번호 설정이 완료되었습니다.`)
+            })
+            .catch((error) => {
+                alert(`비밀번호 설정에 실패했습니다. 다시 시도해 주세요.`)
+            })
+        })()
+    }
 })
